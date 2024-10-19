338f9cc0f9feb32e8ed19dd9e16f8a9d5bc1cf4e
Sergey Yudaev <seyudaev@gmail.com>
2024-10-19 13:36:09 +0300

    init

--- script-pw11.sh
+++ script-pw11.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker run -d -p 9889:80 --name ci-cd-pw11 --mount type=bind,src=/var/lib/jenkins/workspace/sf-ci-pw11/,dst=/usr/share/nginx/html/ nginx
-docker ps -a
-curl -I http://84.201.159.33:9889 2>/dev/null | head -n 1 | cut -d$' ' -f2
-echo -n 'http://84.201.159.33:9889/index.html' | md5sum
-docker stop sf-ci-pw11
-docker rm sf-ci-pw11
-docker ps -a
+sudo docker run -d -p 9889:80 --name ci-cd-pw11 --mount type=bind,src=/var/lib/jenkins/workspace/sf-ci-pw11/,dst=/usr/share/nginx/html/ nginx
+sudo docker ps -a
+sudo curl -I http://84.201.159.33:9889 2>/dev/null | head -n 1 | cut -d$' ' -f2
+sudo echo -n 'http://84.201.159.33:9889/index.html' | md5sum
+sudo docker stop sf-ci-pw11
+sudo docker rm sf-ci-pw11
+sudo docker ps -a
