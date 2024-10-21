0fad640a17e52b17770c5e7fe751e8361ff9bda7
Sergey Yudaev <seyudaev@gmail.com>
2024-10-21 19:57:47 +0300

    fix

--- script-pw11.sh
+++ script-pw11.sh
@@ -2,8 +2,8 @@
 
 sudo docker run -d -p 9889:80 --name sf-ci-pw11 --mount type=bind,src=/var/lib/jenkins/workspace/sf-ci-pw11/,dst=/usr/share/nginx/html/ nginx
 sudo docker ps -a
-sudo curl -I http://84.201.159.33:9889 2>/dev/null | head -n 1 | cut -d$' ' -f2
-sudo echo -n 'http://84.201.159.33:9889/index.html' | openssl md5 -hex
+sudo curl -I http://localhost:9889 2>/dev/null | head -n 1 | cut -d$' ' -f2
+sudo echo -n 'http://localhost:9889/index.html' | openssl md5 -hex
 sudo docker stop sf-ci-pw11
 sudo docker rm sf-ci-pw11
 sudo docker ps -a
